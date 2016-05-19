3e2964a86aef0449e57e215768da857e233a5386
tituspulo <titus.pulo.legion@gmail.com>
2016-05-19 16:23:25 -0700

    make it right

--- get_ticket.py
+++ get_ticket.py
@@ -1,37 +1,22 @@
 import requests
 import json
-import sys
-import urllib3
-from tabulate import tabulate
-
-urllib3.disable_warnings()
 
 
-controller = "sandboxapic.cisco.com:9443"
-username = "admin"
-password = "C!sc0123"
-version = "v1"
-
-def get_aut_ticket(ip=controller,user=username,pwd=password):
-
-    global version
+def get_aut_ticket(ip,user,pwd):
 
     r_json = {
         "username" : user,
         "password" : pwd
     }
 
-    url = "https://"+ip+"/api"+version+"/ticket"
+    url = "https://"+ip+"/api/v1/ticket"
     headers = {'content-type' : 'application/json'}
 
-    try:
-        response = requests.post(url=url, data=json.dumps(r_json), headers=headers, verify=False)
-        return response.json() ["response"]["serviceTicket"]
 
-    except:
-        print("Status: ", response.status_code)
-        print("Response: ", response.text)
-        sys.exit()
+    response = requests.post(url=url, data=json.dumps(r_json), headers=headers, verify=False)
+    return response.json() ["response"]["serviceTicket"]
+
+
 
 
 
--- get_user.py
+++ get_user.py
@@ -1,3 +1,4 @@
+from get_ticket import *
 import requests
 import json
 import urllib3
@@ -8,19 +9,7 @@ controller = "sandboxapic.cisco.com:9443"
 username = "admin"
 password = "C!sc0123"
 version = "v1"
-
-r_json = {
-    "username" : username,
-    "password" : password
-}
-
-post_url = "https://"+controller+"/api/"+version+"/ticket"
-headers = {'content-type' : 'application/json'}
-
-resp = requests.post(url=post_url, data=json.dumps(r_json), headers=headers, verify=False)
-
-ticket_json = resp.json()
-ticket = (ticket_json["response"]["serviceTicket"])
+ticket = get_aut_ticket(controller, username, password)
 
 headers = {'X-Auth-Token' : ticket}
 
